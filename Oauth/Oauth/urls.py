95afc7631255ac1c56a8b2420344a0d4f5fd5bfd
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2023-01-15 15:38:54 +0300

    [fix] some change

--- Oauth/Oauth/urls.py
+++ Oauth/Oauth/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('oauth/', include('oauth.urls')),
+    # path('oauth/', include('oauth.urls')),
     path('accounts/', include('allauth.urls')),
-    
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 ]
